9889eb21fedefd7121c4dd627101c73b606ac2f0
Michael <49486947+RumbleMike@users.noreply.github.com>
2020-12-25 02:06:10 -0500

    Changed Rank name hardcode to semi-cloud based

--- RankDetection.cs
+++ RankDetection.cs
@@ -15,12 +15,15 @@ namespace ValorantStreamOverlay
 {
     class RankDetection
     {
-        
+        List<string> rankNames = new List<string>();
+        private dynamic rankJson;
+
         public RankDetection()
         {
            
             //Start Update
             int rankNum = UPDATECompRankAsync().GetAwaiter().GetResult();
+            GetCloudRankJSON().GetAwaiter().GetResult();
             RankParser(rankNum);
         }
 
@@ -53,114 +56,128 @@ namespace ValorantStreamOverlay
             return 0;
         }
 
+        private async Task GetCloudRankJSON()
+        {
+            IRestClient cloudRankJson = new RestClient(new Uri("https://raw.githubusercontent.com/RumbleMike/ValorantStreamOverlay/main/rankInfo.json"));
+            IRestRequest rankRequest = new RestRequest(Method.GET);
+            IRestResponse rankResp = cloudRankJson.Get(rankRequest);
+            rankJson = (rankResp.IsSuccessful) ? rankJson = rankResp.Content : rankJson = string.Empty;
+        }
         void RankParser(int rankNumber)
         {
+            dynamic ranks = rankJson["Ranks"];
+            string rankNameLower = ranks[rankNumber].ToString();
+            string rankNameALPHA = rankNameLower.ToUpper();
+
+
             Trace.Write("Setting Rank To Valid Rank Num");
 
             switch (rankNumber)
             {
                 case (0):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "UNRATED";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_0;
                     break;
                 case (1):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "UNKNOWN 1";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_1;
                     break;
                 case (2):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "UNKNOWN 2";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_2;
                     break;
                 case (3):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "IRON 1";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_3;
                     break;
                 case (4):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "IRON 2";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_4;
                     break;
                 case (5):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "IRON 3";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_5;
                     break;
                 case (6):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "BRONZE 1";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_6;
                     break;
                 case (7):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "BRONZE 2";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_7;
                     break;
                 case (8):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "BRONZE 3";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_8;
                     break;
                 case (9):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "SILVER 1";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_9;
                     break;
                 case (10):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "SILVER 2";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_10;
                     break;
                 case (11):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "SILVER 3";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_11;
                     break;
                 case (12):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "GOLD 1";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_12;
                     break;
                 case (13):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "GOLD 2";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_13;
                     break;
                 case (14):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "GOLD 3";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_14;
                     break;
                 case (15):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "PLATINUM 1";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_15;
                     break;
                 case (16):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "PLATINUM 2";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_16;
                     break;
                 case (17):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "PLATINUM 3";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_17;
                     break;
                 case (18):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "DIAMOND 1";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_18;
                     break;
                 case (19):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "DIAMOND 2";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_19;
                     break;
                 case (20):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "DIAMOND 3";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_20;
                     break;
                 case (21):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "IMMORTAL 1";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_21;
                     break;
                 case (22):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "IMMORTAL 2";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_22;
                     break;
                 case (23):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "IMMORTAL 3";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_23;
                     break;
                 case (24):
-                    LogicHandler.ValorantOver.rankingLabel.Text = "RADIANT";
+                    LogicHandler.ValorantOver.rankingLabel.Text = rankNameALPHA;
                     LogicHandler.ValorantOver.rankIconBox.Image = Properties.Resources.TX_CompetitiveTier_Large_24;
                     break;
 
             }
         }
+
+       
     }
 }
