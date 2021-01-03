dcdc7786b4811a468967fd30486eb90d49c054f8
Jesús Alejandro Carrillo Arreola <alexcarrilloar@outlook.com>
2021-01-02 20:31:33 -0600

    Changed the settings UI. Added collapsable Twitch Settings. Added color preview in Settings Screen. Fixed all tab indexes in Settings Screen. Added Utils class to manage the color preview in the settings screen.

---
+++ Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValorantStreamOverlay
+{
+    class Utils
+    {
+        public static string COLOR_BLUE = "#0617FC";
+        public static string COLOR_GRAY = "#686868";
+        public static string COLOR_GREEN = "#3EF742";
+        public static string COLOR_LIGHTBLUE = "#1385FA";
+        public static string COLOR_PURPLE = "#7F3EF7";
+        public static string COLOR_RED = "#F72748";
+        public static string COLOR_DEFAULT = "#686868";
+    }
+}
